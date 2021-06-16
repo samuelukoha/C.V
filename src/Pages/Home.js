829095b06bf83104ac427fdf276cfe370f34dacc
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2021-06-16 17:34:14 +0100

    feat:project-setup

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h3>This is the footer</h3>
+        </footer>
+    );
+}
+
+export default Footer;
