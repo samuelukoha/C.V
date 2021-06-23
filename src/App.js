2e81a77df0b42ad229775ff645a33ba354f4c27d
Razaq <razaqonweb@gmail.com>
2021-06-23 20:14:15 +0100

    profile moved to components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Profile from "./pages/Profile/Profile";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
